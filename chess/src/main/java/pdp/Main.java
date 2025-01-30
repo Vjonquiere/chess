92a976161f30b04d354448f85f2d0527463dc37b
iWenn <iwenn.development@gmail.com>
2025-01-30 20:56:21 +0100

    Spotless

--- chess/src/main/java/pdp/Main.java
+++ chess/src/main/java/pdp/Main.java
@@ -2,9 +2,10 @@ package pdp;
 
 public class Main {
 
-  public static String returnsA(){
+  public static String returnsA() {
     return "A";
   }
+
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
--- chess/src/test/java/tests/MainTest.java
+++ chess/src/test/java/tests/MainTest.java
@@ -3,7 +3,6 @@ package tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
 import pdp.Main;
 
 public class MainTest {
