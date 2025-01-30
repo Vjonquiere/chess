f6f32cc88276d570148d63559e527dffe6d2b791
iWenn <iwenn.development@gmail.com>
2025-01-30 20:55:08 +0100

    Adding returnsA test for pipeline

--- chess/src/main/java/pdp/Main.java
+++ chess/src/main/java/pdp/Main.java
@@ -1,6 +1,10 @@
 package pdp;
 
 public class Main {
+
+  public static String returnsA(){
+    return "A";
+  }
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
--- chess/src/test/java/tests/MainTest.java
+++ chess/src/test/java/tests/MainTest.java
@@ -4,10 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import pdp.Main;
+
 public class MainTest {
 
   @Test
   public void voidTest() {
     assertEquals(1, 1);
+  }
+
+  @Test
+  public void equalsA() {
+    assertEquals(Main.returnsA(), "A");
   }
 }
