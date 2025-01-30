fc95e5fc9c35b5d9b065d21f60acca8654bf7470
iWenn <iwenn.development@gmail.com>
2025-01-30 17:57:51 +0100

    Setting up dependencies and test

---
+++ chess/src/test/java/tests/MainTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+  @Test
+  public void voidTest() {
+    assertEquals(1, 1);
+  }
+}
