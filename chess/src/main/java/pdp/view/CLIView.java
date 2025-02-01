42362b470545b0789b259682d33885a7441d1444
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-01 22:20:38 +0100

    Printing game state onGameEvent

--- chess/src/main/java/pdp/view/CLIView.java
+++ chess/src/main/java/pdp/view/CLIView.java
@@ -1,10 +1,10 @@
 package pdp.view;
 
+import pdp.model.Game;
+
 public class CLIView implements View {
   @Override
   public void onGameEvent() {
-    // TODO
-    throw new UnsupportedOperationException(
-        "Method not implemented in " + this.getClass().getName());
+    System.out.println(Game.getInstance().getGameRepresentation());
   }
 }
