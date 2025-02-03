f16278f75bc9b8927bc450d25276a3b9143caaa3
Mathilde Chollon <mathilde.chollon@gmail.com>
2025-02-03 18:50:29 +0100

    added logging + fix option mgmt

--- chess/src/main/java/pdp/Main.java
+++ chess/src/main/java/pdp/Main.java
@@ -1,8 +1,14 @@
 package pdp;
 
+import static pdp.utils.Logging.DEBUG;
+import static pdp.utils.Logging.VERBOSE;
+
+import java.util.logging.Logger;
 import pdp.utils.CLIOptions;
+import pdp.utils.Logging;
 
 public class Main {
+  private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
   public static String returnsA() {
     return "A";
@@ -10,7 +16,9 @@ public class Main {
 
   public static void main(String[] args) {
     CLIOptions.parseOptions(args, Runtime.getRuntime());
+    Logging.configureLogging(LOGGER);
+    DEBUG(LOGGER, "debug test");
+    VERBOSE(LOGGER, "verbose test");
     System.out.println("Hello world!");
-    // TODO
   }
 }
