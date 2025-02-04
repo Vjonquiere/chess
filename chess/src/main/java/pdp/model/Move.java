8fbbc8e8a6e161e43d8c5bf73272605b76ee30d2
Denis Demirci <denis.demirci@etu.u-bordeaux.fr>
2025-02-04 19:37:16 +0100

    Constructor +fromString and fromPosition

--- chess/src/main/java/pdp/model/Move.java
+++ chess/src/main/java/pdp/model/Move.java
@@ -1,5 +1,6 @@
 package pdp.model;
 
+import pdp.exceptions.IllegalMoveException;
 import pdp.utils.Position;
 
 public class Move {
@@ -10,15 +11,43 @@ public class Move {
   boolean isCheck;
   boolean isCheckMate;
 
-  public Move() {
-    // TODO
+  public Move(String move){
+    String[] parts = move.split(" ");
+    this.source = fromString(parts[0]);
+    this.dest = fromString(parts[1]);
+    this.piece = getPieceAt(dest.getX(),dest.getY());
+    this.isTake = false;
+    this.isCheck = false ;
+    this.isCheckMate = false;
+
     throw new UnsupportedOperationException(
         "Method not implemented in " + this.getClass().getName());
   }
 
-  public static Move fromString(String move) {
-    // TODO
-    throw new UnsupportedOperationException("Method not implemented");
+
+  // fromString("e4") -> Position (4,3)
+  public static Position fromString(String move) {
+    
+    char colLetter = Character.toLowerCase(move.charAt(0));
+    int rowNumber = Character.getNumericValue(move.charAt(1));
+
+    if (colLetter < 'a' || colLetter > 'h' || rowNumber < 1 || rowNumber > 8) {
+      throw new IllegalMoveException("Invalid move: " + move);
+    }
+    
+    int x = colLetter - 'a';
+    int y = rowNumber - 1;
+
+    return new Position(y,x);
+  }
+
+  // fromPosition(4,3) -> "e4"
+  public static String fromPosition(Position move) {
+    
+    char colLetter = (char) ('a' + move.getX());
+    int rowNumber = move.getY() - 1;
+
+    return "" + colLetter + rowNumber;
   }
 
   public Position getSource() {
