bff2d37f9cef30954edcc3b9f36f832fce31ef39
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-05 20:30:20 +0100

    Fixing board to ASCII and board constructor

--- chess/src/main/java/pdp/model/Board.java
+++ chess/src/main/java/pdp/model/Board.java
@@ -1,6 +1,6 @@
 package pdp.model;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.logging.Logger;
@@ -19,9 +19,13 @@ public class Board {
 
   public Board() {
     Logging.configureLogging(LOGGER);
-    // TODO
-    throw new UnsupportedOperationException(
-        "Method not implemented in " + this.getClass().getName());
+    this.board = new BitboardRepresentation();
+    this.isWhite = true;
+    this.enPassant = 0;
+    this.whiteShortCastle = true;
+    this.blackShortCastle = true;
+    this.whiteLongCastle = true;
+    this.blackLongCastle = true;
   }
 
   public List<Move> getAvailableMoves() {
@@ -40,52 +44,42 @@ public class Board {
   }
 
   public String getAsciiRepresentation() {
+    int rows = this.board.getNbRows();
+    int cols = this.board.getNbCols();
+    char[][] charBoard = new char[rows][cols];
 
-    ArrayList<ArrayList<Character>> charBoard = new ArrayList<ArrayList<Character>>();
-    for (int i = 0; i < this.board.getNbCols(); i++) {
-      charBoard.add(new ArrayList<Character>());
-      for (int j = 0; j < this.board.getNbRows(); j++) {
-        charBoard.get(i).add('_');
-      }
+    for (int i = 0; i < rows; i++) {
+      Arrays.fill(charBoard[i], '_');
     }
 
     for (int i = 0; i < 2; i++) {
-      boolean color = i == 0;
-      char rep = Piece.PAWN.getCharRepresentation(color);
-      for (Position pos : this.board.getPawns(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
-      rep = Piece.ROOK.getCharRepresentation(color);
-      for (Position pos : this.board.getRooks(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
-      rep = Piece.KNIGHT.getCharRepresentation(color);
-      for (Position pos : this.board.getKnights(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
-      rep = Piece.BISHOP.getCharRepresentation(color);
-      for (Position pos : this.board.getBishops(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
-      rep = Piece.QUEEN.getCharRepresentation(color);
-      for (Position pos : this.board.getQueens(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
-      rep = Piece.KING.getCharRepresentation(color);
-      for (Position pos : this.board.getKing(color)) {
-        charBoard.get(pos.getY()).set(pos.getX(), rep);
-      }
+      boolean color = (i == 0);
+
+      placePiecesOnBoard(
+          charBoard, this.board.getPawns(color), Piece.PAWN.getCharRepresentation(color));
+      placePiecesOnBoard(
+          charBoard, this.board.getRooks(color), Piece.ROOK.getCharRepresentation(color));
+      placePiecesOnBoard(
+          charBoard, this.board.getKnights(color), Piece.KNIGHT.getCharRepresentation(color));
+      placePiecesOnBoard(
+          charBoard, this.board.getBishops(color), Piece.BISHOP.getCharRepresentation(color));
+      placePiecesOnBoard(
+          charBoard, this.board.getQueens(color), Piece.QUEEN.getCharRepresentation(color));
+      placePiecesOnBoard(
+          charBoard, this.board.getKing(color), Piece.KING.getCharRepresentation(color));
     }
 
     StringJoiner sj = new StringJoiner("\n");
-    for (ArrayList<Character> row : charBoard) {
-      StringBuilder rowSb = new StringBuilder();
-      for (Character cell : row) {
-        rowSb.append(cell);
-      }
-      sj.add(rowSb.toString());
+    for (char[] row : charBoard) {
+      sj.add(new String(row));
     }
 
-    return (sj.toString());
+    return sj.toString();
+  }
+
+  private void placePiecesOnBoard(char[][] board, List<Position> positions, char rep) {
+    for (Position pos : positions) {
+      board[this.board.getNbRows() - 1 - pos.getY()][pos.getX()] = rep;
+    }
   }
 }
