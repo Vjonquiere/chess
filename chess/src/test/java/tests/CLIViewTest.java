cb9c16e493200bef3e0768eda5279dbc6205d803
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-05 20:35:53 +0100

    Adding test for Board Display (Close #14)

--- chess/src/test/java/tests/CLIViewTest.java
+++ chess/src/test/java/tests/CLIViewTest.java
@@ -1,22 +1,24 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import pdp.model.Game;
+
 public class CLIViewTest {
-  // TODO remove comment
-  /*
   @Test
   public void testBoardToASCII(){
     Game game = Game.getInstance();
 
     String expected = "rnbqkbnr"+
     "\npppppppp"+
-    "\n........"+
-    "\n........"+
-    "\n........"+
-    "\n........"+
+    "\n________"+
+    "\n________"+
+    "\n________"+
+    "\n________"+
     "\nPPPPPPPP"+
     "\nRNBQKBNR";
 
     assertEquals(expected, game.getBoard().getAsciiRepresentation());
   }
-    */
 }
