d6bf723a4f581f0212cc2f6f2dd6e875aff0e43e
Jonathan Landry <jonathan.landry@etu.u-bordeaux.fr>
2025-02-06 09:49:44 +0100

    Adding first tests for PawnPromotion (method isPawnPromoting)

--- chess/src/test/java/tests/BitboardRepresentationTest.java
+++ chess/src/test/java/tests/BitboardRepresentationTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -36,5 +37,51 @@ public class BitboardRepresentationTest {
     // board.getPawnMoves(new Position(1,0), new Bitboard(0L), new Bitboard(1157443791906541584L));
     // board.getQueenMoves(new Position(4,4), new Bitboard(34628173824L), new
     // Bitboard(17609500131328L));
+  }
+
+  @Test
+  public void testIsPawnPromotingShouldReturnFalse() {
+    BitboardRepresentation board = new BitboardRepresentation();
+    boolean resultWhite;
+    boolean resultBlack;
+    boolean white = true;
+    int xForWhite = 0, yForWhite = 0;
+    int xForBlack = 0, yForBlack = 7;
+    int nbLoops = 7;
+
+    for (int i = 0; i < nbLoops; i++) {
+      resultWhite = board.isPawnPromoting(xForWhite, yForWhite, white);
+      resultBlack = board.isPawnPromoting(xForBlack, yForBlack, !white);
+      assertFalse(resultWhite, "Pawn should not be able to promote !");
+      assertFalse(resultBlack, "Pawn should not be able to promote !");
+      yForWhite++;
+      yForBlack--;
+    }
+  }
+
+  @Test
+  public void testIsPawnPromotingShouldReturnTrue() {
+    /*
+    BitboardRepresentation board = new BitboardRepresentation();
+
+    int xWhite = 0, yWhite = 7;
+    boolean white = true;
+
+    // TO DO
+    // Make pawn move so that it reaches its last rank
+
+    boolean resultWhite = board.isPawnPromoting(xWhite, yWhite, white);
+    assertTrue(resultWhite, "White pawn should be able to promote!");
+
+    int xBlack = 0, yBlack = 0;
+
+    // TO DO
+    // Make pawn move so that it reaches its last rank
+
+    boolean resultBlack = board.isPawnPromoting(xBlack, yBlack, !white);
+    assertTrue(resultBlack, "Black pawn should be able to promote!");
+    */
+
+    assertFalse(false);
   }
 }
