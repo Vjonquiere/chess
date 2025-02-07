654ad32e2321da7fd6cc7f902d01f27d5e71f13a
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-07 09:51:08 +0100

    Updating hook

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ ! -f .git/hooks/pre-commit ]; then
-    echo "Setting up Git hooks..."
+echo "Setting up Git hooks..."
 
-    HOOKS_DIR=".git/hooks"
-    cp hooks/pre-commit "$HOOKS_DIR/pre-commit"
-    chmod +x "$HOOKS_DIR/pre-commit"
+HOOKS_DIR=".git/hooks"
+cp hooks/pre-commit "$HOOKS_DIR/pre-commit"
+chmod +x "$HOOKS_DIR/pre-commit"
 
-    echo "Git hooks installed!"
-else
-    echo "Pre-commit hook already set up."
-fi
+echo "Git hooks installed!"
