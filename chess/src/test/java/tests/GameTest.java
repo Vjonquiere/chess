e90c59b61130ee441649280cae7b2ed37428160e
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-02-11 19:46:50 +0100

    Game playMove tests

--- chess/src/test/java/tests/GameTest.java
+++ chess/src/test/java/tests/GameTest.java
@@ -3,6 +3,7 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import pdp.exceptions.IllegalMoveException;
 import pdp.model.BitboardRepresentation;
 import pdp.model.Game;
 import pdp.model.Move;
@@ -19,14 +20,108 @@ public class GameTest {
     bitboards.movePiece(new Position(1, 0), new Position(2, 0));
     assertEquals(game.getBoard().getBoard(), bitboards);
 
-    // Play with wrong player
-    // assertThrows()
-    // game.playMove(new Move(new Position(2,0), new Position(3,0)));
-
     // Play move only in game
     game.playMove(new Move(new Position(6, 1), new Position(5, 1)));
     assertNotEquals(game.getBoard().getBoard(), bitboards);
     bitboards.movePiece(new Position(6, 1), new Position(5, 1)); // Play move in bitboards
     assertEquals(game.getBoard().getBoard(), bitboards);
+
+    // Tests to add
+    // 6. try a move that is not a classical or special move
+  }
+
+  @Test
+  public void playEnPassantTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 0), new Position(3, 0)));
+    game.playMove(new Move(new Position(6, 1), new Position(4, 1)));
+    game.playMove(new Move(new Position(3, 0), new Position(4, 1)));
+    BitboardRepresentation bitboards = new BitboardRepresentation();
+    bitboards.movePiece(new Position(1, 0), new Position(4, 1));
+    bitboards.deletePieceAt(1, 6);
+    assertEquals(bitboards, game.getBoard().getBoard());
+  }
+
+  @Test
+  public void playEnPassantIsCheckTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 4), new Position(2, 4)));
+    game.playMove(new Move(new Position(6, 7), new Position(4, 7)));
+    game.playMove(new Move(new Position(2, 4), new Position(3, 4)));
+    game.playMove(new Move(new Position(7, 7), new Position(5, 7)));
+    game.playMove(new Move(new Position(3, 4), new Position(4, 4)));
+    game.playMove(new Move(new Position(5, 7), new Position(5, 4)));
+    game.playMove(new Move(new Position(1, 7), new Position(2, 7)));
+    game.playMove(new Move(new Position(6, 3), new Position(4, 3)));
+
+    assertThrows(
+        IllegalMoveException.class,
+        () -> {
+          game.playMove(new Move(new Position(4, 4), new Position(5, 3)));
+        });
+  }
+
+  @Test
+  public void playDoublePushTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 1), new Position(3, 1)));
+    BitboardRepresentation bitboards = new BitboardRepresentation();
+    bitboards.movePiece(new Position(1, 1), new Position(3, 1));
+    assertEquals(bitboards, game.getBoard().getBoard());
+  }
+
+  @Test
+  public void playDoublePushIsCheckTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 0), new Position(2, 0)));
+    game.playMove(new Move(new Position(6, 3), new Position(4, 3)));
+    game.playMove(new Move(new Position(2, 0), new Position(3, 0)));
+    game.playMove(new Move(new Position(7, 3), new Position(6, 3)));
+    game.playMove(new Move(new Position(3, 0), new Position(4, 0)));
+    game.playMove(new Move(new Position(6, 3), new Position(5, 3)));
+    game.playMove(new Move(new Position(1, 5), new Position(3, 5)));
+
+    assertThrows(
+        IllegalMoveException.class,
+        () -> {
+          game.playMove(new Move(new Position(1, 4), new Position(3, 4)));
+        });
+  }
+
+  @Test
+  public void wrongTurnForWhitePlayerTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 0), new Position(2, 0)));
+    assertThrows(
+        IllegalMoveException.class,
+        () -> {
+          game.playMove(new Move(new Position(2, 0), new Position(3, 0)));
+        });
+  }
+
+  @Test
+  public void wrongTurnForBlackPlayerTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 0), new Position(2, 0)));
+    game.playMove(new Move(new Position(6, 0), new Position(5, 0)));
+    assertThrows(
+        IllegalMoveException.class,
+        () -> {
+          game.playMove(new Move(new Position(5, 0), new Position(4, 0)));
+        });
+  }
+
+  @Test
+  public void pinnedPieceTest() {
+    Game game = Game.initialize(false, false, null, null);
+    game.playMove(new Move(new Position(1, 0), new Position(2, 0)));
+    game.playMove(new Move(new Position(6, 2), new Position(4, 2)));
+    game.playMove(new Move(new Position(2, 0), new Position(3, 0)));
+    game.playMove(new Move(new Position(7, 3), new Position(4, 0)));
+    assertThrows(
+        IllegalMoveException.class,
+        () -> {
+          game.playMove(new Move(new Position(1, 3), new Position(2, 3)));
+        });
   }
 }
