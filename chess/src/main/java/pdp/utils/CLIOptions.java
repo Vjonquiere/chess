503a70bc4f815e786f112657ad25c1f45167fd94
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-14 10:57:08 +0100

    Refactoring CLIOptions with default

--- chess/src/main/java/pdp/utils/CLIOptions.java
+++ chess/src/main/java/pdp/utils/CLIOptions.java
@@ -9,281 +9,147 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class CLIOptions {
   private static final Logger LOGGER = Logger.getLogger(CLIOptions.class.getName());
 
-  /*Private constructor to avoid instantiation*/
   private CLIOptions() {}
-  ;
 
-  /**
-   * Parse the options given in parameters. The help will have the priority over any other option.
-   * If the usage is wrong, an error message will be displayed on the standard error output.
-   * Careful, partial matching : -aifdghj will still enable -a, no errors are displayed.
-   *
-   * @param args Array of strings received by main, arguments to parse
-   * @param runtime Runtime to exit cleanly
-   */
   public static HashMap<OptionType, String> parseOptions(String[] args, Runtime runtime) {
-    final Options options = new Options();
-    Option help = new Option("h", "help", false, "Print this message and exit");
-    Option version = new Option("V", "version", false, "Print the version information and exit");
-    Option verbose = new Option("v", "verbose", false, "Display more information");
-    Option debug = new Option("d", "debug", false, "Print debugging information");
-    Option blitz = new Option("b", "blitz", false, "Play in blitz mode");
-    Option gui = new Option("g", "gui", false, "Displays the game with a  graphical interface.");
-    Option time =
-        Option.builder("t")
-            .longOpt("time")
-            .hasArg(true)
-            .argName("TIME")
-            .desc("Specify time per round for blitz mode (default 30min)")
-            .type(Integer.class)
-            .build();
-    Option contest =
-        Option.builder("c")
-            .longOpt("contest")
-            .hasArg(true)
-            .argName("FILENAME")
-            .desc("AI plays one move in the given file")
-            .build();
-    Option ai =
-        Option.builder("a")
-            .longOpt("ai")
-            .optionalArg(true)
-            .argName("COLOR")
-            .desc(
-                "Launch the program in AI mode, with artificial player with COLOR 'B' or 'A' (All),(W by default).")
-            .build();
-    Option ai_mode =
-        Option.builder()
-            .longOpt("ai-mode")
-            .hasArg(true)
-            .argName("ALGORITHM")
-            .desc("Choose the exploration algorithm for the artificial player.")
-            .build();
-    Option ai_depth =
-        Option.builder()
-            .longOpt("ai-depth")
-            .hasArg(true)
-            .argName("DEPTH")
-            .desc("Specify the depth of the AI algorithm")
-            .build();
-    Option ai_heuristic =
-        Option.builder()
-            .longOpt("ai-heuristic")
-            .hasArg(true)
-            .argName("HEURISTIC")
-            .desc("Choose the heuristic for the artificial player")
-            .build();
-    Option ai_time =
-        Option.builder()
-            .longOpt("ai-time")
-            .argName("TIME")
-            .hasArg(true)
-            .desc("Specify the time of reflexion for AI mode (default 5 seconds)")
-            .build();
-    Option lang =
-        Option.builder()
-            .longOpt("lang")
-            .argName("LANGUAGE")
-            .hasArg(true)
-            .desc("Choose the language for the app (en supported)")
-            .build();
-
-    options.addOption(help);
-    options.addOption(version);
-    options.addOption(verbose);
-    options.addOption(debug);
-    options.addOption(blitz);
-    options.addOption(gui);
-    options.addOption(time);
-    options.addOption(contest);
-    options.addOption(ai);
-    options.addOption(ai_mode);
-    options.addOption(ai_depth);
-    options.addOption(ai_heuristic);
-    options.addOption(ai_time);
-    options.addOption(lang);
-
-    Map<String, String> defaultArgs;
-    try {
-      InputStream inputStream =
-          CLIOptions.class.getClassLoader().getResourceAsStream("config.chessrc");
-      if (inputStream == null) {
-        throw new FileNotFoundException("config.chessrc not found in classpath!");
-      }
-      Map<String, Map<String, String>> iniMap = IniParser.parseIni(inputStream);
-      defaultArgs = iniMap.get("Default");
-    } catch (Exception e) {
-      System.err.println("Error while parsing chessrc file: " + e.getMessage());
-      defaultArgs = new HashMap<>();
+    Options options = new Options();
+    for (OptionType optionType : OptionType.values()) {
+      options.addOption(optionType.getOption());
     }
 
+    Map<String, String> defaultArgs = loadDefaultArgs();
     CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = null;
     HashMap<OptionType, String> activatedOptions = new HashMap<>();
+
     try {
-      cmd = parser.parse(options, args);
-
-      if (cmd.hasOption(debug)
-          || defaultArgs.containsKey("debug") && defaultArgs.get("debug").equals("true")) {
-        Logging.setDebug(true);
-        Logging.configureLogging(LOGGER);
-        DEBUG(LOGGER, "Debug mode activated");
-      }
-      if (cmd.hasOption(verbose)
-          || defaultArgs.containsKey("verbose") && defaultArgs.get("verbose").equals("true")) {
-        Logging.setVerbose(true);
-        Logging.configureLogging(LOGGER);
-        DEBUG(LOGGER, "Verbose mode activated");
-      }
-      if (cmd.hasOption(help)) {
-        DEBUG(LOGGER, "Help option activated");
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("chess", options);
-        runtime.exit(0);
-        return null;
-      }
-      if (cmd.hasOption(version)) {
-        DEBUG(LOGGER, "Version option activated");
-        final Properties properties = new Properties();
-        properties.load(CLIOptions.class.getClassLoader().getResourceAsStream(".properties"));
-        System.out.println("Version: " + properties.getProperty("version"));
-        runtime.exit(0);
-        return null;
-      }
-      if (cmd.hasOption(lang)) {
-        DEBUG(LOGGER, "Language option activated");
-        if (cmd.getParsedOptionValue(lang).equals("en")) {
-          DEBUG(LOGGER, "Language = English (already set by default)");
-          // TODO: de-comment when french file finished
-        } /*else if (cmd.getParsedOptionValue(lang).equals("fr")) {
-            DEBUG(LOGGER, "Language = French");
-            TextGetter.setLocale(cmd.getParsedOptionValue("lang"));
-          } */ else {
-          System.err.println(
-              "Language " + cmd.getParsedOptionValue(lang) + " not supported, language = english");
-        }
-      }
-      if (cmd.hasOption(blitz)
-          || defaultArgs.containsKey("blitz") && defaultArgs.get("blitz").equals("true")) {
-        DEBUG(LOGGER, "Blitz mode activated");
-        activatedOptions.put(OptionType.BLITZ, "");
-        System.err.println("Blitz not implemented yet");
-      }
-      if (cmd.hasOption(gui)) {
-        DEBUG(LOGGER, "GUI mode activated");
-        activatedOptions.put(OptionType.GUI, "");
-        System.err.println("GUI not implemented yet");
-      }
-      if (cmd.hasOption(time)) {
-        DEBUG(LOGGER, "Blitz time option activated");
-        activatedOptions.put(OptionType.TIME, cmd.getOptionValue(time));
-        System.err.println("Blitz time not implemented yet");
-      } else if (defaultArgs.containsKey("time")) {
-        DEBUG(LOGGER, "Blitz time option activated");
-        activatedOptions.put(OptionType.TIME, defaultArgs.get("time"));
-        System.err.println("Blitz time not implemented yet");
-      }
-      if (cmd.hasOption(contest)) {
-        DEBUG(LOGGER, "Contest mode activated");
-        activatedOptions.put(OptionType.CONTEST, cmd.getOptionValue(contest));
-        System.err.println("Contest not implemented yet");
-      }
-      if (cmd.hasOption(ai)) {
-        DEBUG(LOGGER, "AI activated");
-        activatedOptions.put(OptionType.AI, cmd.getOptionValue(ai));
-        System.err.println("AI not implemented yet");
-      } else if (defaultArgs.containsKey("ai") && !defaultArgs.get("ai").equals("false")) {
-        DEBUG(LOGGER, "AI activated");
-        activatedOptions.put(OptionType.AI, defaultArgs.get("ai"));
-        System.err.println("AI not implemented yet");
-      }
-
-      if (cmd.hasOption(ai_mode)) {
-        if (!activatedOptions.containsKey(OptionType.AI)) {
-          System.err.println("Modifying the AI algorithm requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-mode activated");
-          activatedOptions.put(OptionType.AI_MODE, cmd.getOptionValue(ai_mode));
-          System.err.println("AI mode not implemented yet");
-        }
-      } else if (defaultArgs.containsKey("ai-mode")) {
-        if (!activatedOptions.containsKey(OptionType.AI)) {
-          System.err.println("Modifying the AI algorithm requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-mode activated");
-          activatedOptions.put(OptionType.AI_MODE, defaultArgs.get("ai-mode"));
-          System.err.println("AI mode not implemented yet");
-        }
-      }
-
-      if (cmd.hasOption(ai_heuristic)) {
-        if (!cmd.hasOption(ai)) {
-          System.err.println("Choosing the AI heuristic requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-heuristic activated");
-          activatedOptions.put(OptionType.AI_HEURISTIC, cmd.getOptionValue(ai_heuristic));
-          System.err.println("AI mode not implemented yet");
-        }
-      } else if (defaultArgs.containsKey("ai-heuristic")) {
-        if (!activatedOptions.containsKey(OptionType.AI)) {
-          System.err.println("Modifying the AI algorithm requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-heuristic activated");
-          activatedOptions.put(OptionType.AI_HEURISTIC, defaultArgs.get("ai-heuristic"));
-          System.err.println("AI mode not implemented yet");
-        }
-      }
-
-      if (cmd.hasOption(ai_depth)) {
-        if (!cmd.hasOption(ai)) {
-          System.err.println("Modifying the AI depth requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-depth activated");
-          activatedOptions.put(OptionType.AI_DEPTH, cmd.getOptionValue(ai_depth));
-          System.err.println("AI mode not implemented yet");
-        }
-      } else if (defaultArgs.containsKey("ai-depth")) {
-        if (!activatedOptions.containsKey(OptionType.AI)) {
-          System.err.println("Modifying the AI algorithm requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-mode activated");
-          activatedOptions.put(OptionType.AI_DEPTH, defaultArgs.get("ai-depth"));
-          System.err.println("AI mode not implemented yet");
-        }
-      }
-
-      if (cmd.hasOption(ai_time)) {
-        if (!cmd.hasOption(ai)) {
-          System.err.println("Modifying the AI time requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-time activated");
-          activatedOptions.put(OptionType.AI_TIME, cmd.getOptionValue(ai_time));
-          System.err.println("AI mode not implemented yet");
-        }
-      } else if (defaultArgs.containsKey("ai-time")) {
-        if (!activatedOptions.containsKey(OptionType.AI)) {
-          System.err.println("Modifying the AI algorithm requires 'a' argument");
-        } else {
-          DEBUG(LOGGER, "AI-mode activated");
-          activatedOptions.put(OptionType.AI_TIME, defaultArgs.get("ai-time"));
-          System.err.println("AI mode not implemented yet");
-        }
-      }
-
+      CommandLine cmd = parser.parse(options, args);
+      handleLoggingOptions(cmd, defaultArgs);
+      if (handleImmediateExitOptions(cmd, options, runtime)) return null;
+      processOptions(cmd, defaultArgs, activatedOptions);
     } catch (ParseException exp) {
       System.out.println("Parsing failed.  Reason: " + exp.getMessage());
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.printHelp("chess", options);
+      new HelpFormatter().printHelp("chess", options);
       runtime.exit(1);
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-
     return activatedOptions;
+  }
+
+  private static Map<String, String> loadDefaultArgs() {
+    try (InputStream inputStream =
+        CLIOptions.class.getClassLoader().getResourceAsStream("config.chessrc")) {
+      if (inputStream == null)
+        throw new FileNotFoundException("config.chessrc not found in classpath!");
+      return IniParser.parseIni(inputStream).getOrDefault("Default", new HashMap<>());
+    } catch (Exception e) {
+      System.err.println("Error while parsing chessrc file: " + e.getMessage());
+      return new HashMap<>();
+    }
+  }
+
+  private static void handleLoggingOptions(CommandLine cmd, Map<String, String> defaultArgs) {
+    if (cmd.hasOption(OptionType.DEBUG.getLong()) || "true".equals(defaultArgs.get("debug"))) {
+      Logging.setDebug(true);
+      Logging.configureLogging(LOGGER);
+      DEBUG(LOGGER, "Debug mode activated");
+    }
+    if (cmd.hasOption(OptionType.VERBOSE.getLong()) || "true".equals(defaultArgs.get("verbose"))) {
+      Logging.setVerbose(true);
+      Logging.configureLogging(LOGGER);
+      DEBUG(LOGGER, "Verbose mode activated");
+    }
+  }
+
+  private static boolean handleImmediateExitOptions(
+      CommandLine cmd, Options options, Runtime runtime) throws IOException {
+    if (cmd.hasOption(OptionType.HELP.getLong())) {
+      DEBUG(LOGGER, "Help option activated");
+      new HelpFormatter().printHelp("chess", options);
+      runtime.exit(0);
+      return true;
+    }
+    if (cmd.hasOption(OptionType.VERSION.getLong())) {
+      DEBUG(LOGGER, "Version option activated");
+      Properties properties = new Properties();
+      properties.load(CLIOptions.class.getClassLoader().getResourceAsStream(".properties"));
+      System.out.println("Version: " + properties.getProperty("version"));
+      runtime.exit(0);
+      return true;
+    }
+    return false;
+  }
+
+  private static void processOptions(
+      CommandLine cmd,
+      Map<String, String> defaultArgs,
+      HashMap<OptionType, String> activatedOptions) {
+    for (OptionType option : OptionType.values()) {
+
+      boolean userProvided = cmd.hasOption(option.getLong());
+      boolean defaultEnabled =
+          defaultArgs.containsKey(option.getLong())
+              && !"false".equals(defaultArgs.get(option.getLong()));
+
+      if (userProvided || defaultEnabled) {
+        String value =
+            userProvided
+                ? cmd.getOptionValue(option.getLong(), "")
+                : defaultArgs.get(option.getLong());
+        activatedOptions.put(option, value != null ? value : "");
+        DEBUG(LOGGER, option.getLong() + " option activated");
+
+        if (option == OptionType.LANG) {
+          if (value.equals("en")) {
+            DEBUG(LOGGER, "Language = English (already set by default)");
+            // TODO: de-comment when french file finished
+          } /*else if (value.equals("fr")) {
+              DEBUG(LOGGER, "Language = French");
+              TextGetter.setLocale(cmd.getParsedOptionValue("lang"));
+            } */ else {
+            System.err.println(
+                "Language "
+                    + cmd.getOptionValue(option.getLong(), "")
+                    + " not supported, language = english");
+          }
+        }
+
+        if (!isFeatureImplemented(option)) {
+          System.err.println(option.getLong() + " not implemented yet");
+        }
+      }
+    }
+    validateAIOptions(cmd, activatedOptions);
+  }
+
+  private static void validateAIOptions(
+      CommandLine cmd, HashMap<OptionType, String> activatedOptions) {
+    if (!activatedOptions.containsKey(OptionType.AI)) {
+      for (OptionType aiOption :
+          new OptionType[] {
+            OptionType.AI_MODE, OptionType.AI_DEPTH, OptionType.AI_HEURISTIC, OptionType.AI_TIME
+          }) {
+        if (cmd.hasOption(aiOption.getLong())) {
+          System.err.println("Modifying " + aiOption.getLong() + " requires 'a' argument");
+        }
+      }
+    }
+  }
+
+  private static boolean isFeatureImplemented(OptionType option) {
+    return switch (option) {
+      case BLITZ, GUI, TIME, CONTEST, AI, AI_MODE, AI_DEPTH, AI_HEURISTIC, AI_TIME -> false;
+      default -> true;
+    };
   }
 }
--- chess/src/main/java/pdp/utils/OptionType.java
+++ chess/src/main/java/pdp/utils/OptionType.java
@@ -1,13 +1,280 @@
 package pdp.utils;
 
+import org.apache.commons.cli.Option;
+
 public enum OptionType {
-  BLITZ,
-  GUI,
-  TIME,
-  CONTEST,
-  AI,
-  AI_MODE,
-  AI_DEPTH,
-  AI_HEURISTIC,
-  AI_TIME;
+  LANG {
+    @Override
+    public String getShort() {
+      return null;
+    }
+
+    @Override
+    public String getLong() {
+      return "lang";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder()
+          .longOpt(this.getLong())
+          .argName("LANGUAGE")
+          .hasArg(true)
+          .desc("Choose the language for the app (en supported)")
+          .build();
+    }
+  },
+  HELP {
+    @Override
+    public String getShort() {
+      return "h";
+    }
+
+    @Override
+    public String getLong() {
+      return "help";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(this.getShort(), this.getLong(), false, "Print this message and exit");
+    }
+  },
+  VERSION {
+    @Override
+    public String getShort() {
+      return "V";
+    }
+
+    @Override
+    public String getLong() {
+      return "version";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(
+          this.getShort(), this.getLong(), false, "Print the version information and exit");
+    }
+  },
+  VERBOSE {
+    @Override
+    public String getShort() {
+      return "v";
+    }
+
+    @Override
+    public String getLong() {
+      return "verbose";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(this.getShort(), this.getLong(), false, "Display more information");
+    }
+  },
+  DEBUG {
+    @Override
+    public String getShort() {
+      return "d";
+    }
+
+    @Override
+    public String getLong() {
+      return "debug";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(this.getShort(), this.getLong(), false, "Print debugging information");
+    }
+  },
+  BLITZ {
+    @Override
+    public String getShort() {
+      return "b";
+    }
+
+    @Override
+    public String getLong() {
+      return "blitz";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(this.getShort(), this.getLong(), false, "Play in blitz mode");
+    }
+  },
+  GUI {
+    @Override
+    public String getShort() {
+      return "g";
+    }
+
+    @Override
+    public String getLong() {
+      return "gui";
+    }
+
+    @Override
+    public Option getOption() {
+      return new Option(
+          this.getShort(), this.getLong(), false, "Displays the game with a  graphical interface.");
+    }
+  },
+  TIME {
+    @Override
+    public String getShort() {
+      return "t";
+    }
+
+    @Override
+    public String getLong() {
+      return "time";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder(this.getShort())
+          .longOpt(this.getLong())
+          .hasArg(true)
+          .argName("TIME")
+          .desc("Specify time per round for blitz mode (default 30min)")
+          .type(Integer.class)
+          .build();
+    }
+  },
+  CONTEST {
+    @Override
+    public String getShort() {
+      return "c";
+    }
+
+    @Override
+    public String getLong() {
+      return "contest";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder(this.getShort())
+          .longOpt(this.getLong())
+          .hasArg(true)
+          .argName("FILENAME")
+          .desc("AI plays one move in the given file")
+          .build();
+    }
+  },
+  AI {
+    @Override
+    public String getShort() {
+      return "a";
+    }
+
+    @Override
+    public String getLong() {
+      return "ai";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder(this.getShort())
+          .longOpt(this.getLong())
+          .optionalArg(true)
+          .argName("COLOR")
+          .desc(
+              "Launch the program in AI mode, with artificial player with COLOR ’B’ or ’A’ (All),(W by default).")
+          .build();
+    }
+  },
+  AI_MODE {
+    @Override
+    public String getShort() {
+      return null;
+    }
+
+    @Override
+    public String getLong() {
+      return "ai-mode";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder()
+          .longOpt(this.getLong())
+          .hasArg(true)
+          .argName("ALGORITHM")
+          .desc("Choose the exploration algorithm for the artificial player.")
+          .build();
+    }
+  },
+  AI_DEPTH {
+    @Override
+    public String getShort() {
+      return null;
+    }
+
+    @Override
+    public String getLong() {
+      return "ai-depth";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder()
+          .longOpt(this.getLong())
+          .hasArg(true)
+          .argName("DEPTH")
+          .desc("Specify the depth of the AI algorithm")
+          .build();
+    }
+  },
+  AI_HEURISTIC {
+    @Override
+    public String getShort() {
+      return null;
+    }
+
+    @Override
+    public String getLong() {
+      return "ai-heuristic";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder()
+          .longOpt(this.getLong())
+          .hasArg(true)
+          .argName("HEURISTIC")
+          .desc("Choose the heuristic for the artificial player")
+          .build();
+    }
+  },
+  AI_TIME {
+    @Override
+    public String getShort() {
+      return null;
+    }
+
+    @Override
+    public String getLong() {
+      return "ai-time";
+    }
+
+    @Override
+    public Option getOption() {
+      return Option.builder()
+          .longOpt(this.getLong())
+          .argName("TIME")
+          .hasArg(true)
+          .desc("Specify the time of reflexion for AI mode (default 5 seconds)")
+          .build();
+    }
+  };
+
+  public abstract String getShort();
+
+  public abstract String getLong();
+
+  public abstract Option getOption();
 }
