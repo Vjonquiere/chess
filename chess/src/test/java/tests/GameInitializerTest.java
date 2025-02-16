d481b170ee99f586cddb747d1c144f55309d121d
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-02-16 18:22:19 +0100

    Adding GameInitializer tests

--- chess/src/test/java/tests/GameInitializerTest.java
+++ chess/src/test/java/tests/GameInitializerTest.java
@@ -3,20 +3,39 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.HashMap;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pdp.GameInitializer;
 import pdp.controller.GameController;
+import pdp.model.board.Move;
+import pdp.model.piece.Piece;
 import pdp.utils.OptionType;
+import pdp.utils.Position;
 import pdp.view.CLIView;
 
 class GameInitializerTest {
   private HashMap<OptionType, String> options;
+  private Path tempFile;
 
   @BeforeEach
   void setUp() {
     options = new HashMap<>();
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    // Clean up temporary file if it was created.
+    if (tempFile != null && Files.exists(tempFile)) {
+      Files.delete(tempFile);
+    }
   }
 
   @Test
@@ -80,4 +99,46 @@ class GameInitializerTest {
       assertNotNull(controller);
     }
   */
+
+  @Test
+  void testGameInitializationLoadSuccess() throws IOException {
+    tempFile = Files.createTempFile("moveHistory", ".txt");
+    Files.write(tempFile, Arrays.asList("e2-e4"));
+
+    options.put(OptionType.LOAD, tempFile.toString());
+
+    GameController controller = GameInitializer.initialize(options);
+
+    Position newPosition = Move.stringToPosition("e4");
+
+    assertNotNull(controller);
+    assertEquals(
+        controller
+            .getModel()
+            .getBoard()
+            .getBoardRep()
+            .getPieceAt(newPosition.getX(), newPosition.getY())
+            .piece,
+        Piece.PAWN);
+  }
+
+  @Test
+  void testGameInitializationLoadFallback() {
+    options.put(OptionType.LOAD, "non_existent_file.txt");
+
+    ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    PrintStream originalErr = System.err;
+    System.setErr(new PrintStream(errContent));
+
+    GameController controller = null;
+    controller = GameInitializer.initialize(options);
+
+    String errorOutput = errContent.toString();
+
+    System.setErr(originalErr);
+
+    assertTrue(errorOutput.contains("Using the default game start"));
+
+    assertNotNull(controller);
+  }
 }
