fd0165a1d6b55625191aa045afe0a4175a4a1746
Jonathan Landry <jonathan.landry@etu.u-bordeaux.fr>
2025-02-23 11:22:04 +0100

    Pushing MCTS branch

--- chess/src/main/java/pdp/model/ai/algorithms/MCTS.java
+++ chess/src/main/java/pdp/model/ai/algorithms/MCTS.java
@@ -15,4 +15,23 @@ public class MCTS implements SearchAlgorithm {
   public AIMove findBestMove(Game game, int depth, boolean player) {
     return null;
   }
+
+  // Selection
+
+  // Expansion
+
+  // Simulation
+
+  // BackPropagation
+
+  // To know which node to visit next, use UCT (Upper Confidence bound applied to Trees).
+  // w(i)/n(i) + c * sqrt(ln(t)/n(i))
+
+  // w = number of wins after the i-th move (can be 0)
+  // n = number of simulations after the i-th move
+  // c = exploration parameter
+  // t = total number of simulations for the parent node
+
+  // Basically we want to retain some aspect of "history" in the decision making
+  // And expand the exploration of possibilities
 }
