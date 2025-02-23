f534413656e83e93e4f8525a2b8e8209f474fd7b
Jonathan Landry <jonathan.landry@etu.u-bordeaux.fr>
2025-02-23 17:48:43 +0100

    Adding getters and utils methods to TreeNode class

--- chess/src/main/java/pdp/model/ai/algorithms/TreeNodeMCTS.java
+++ chess/src/main/java/pdp/model/ai/algorithms/TreeNodeMCTS.java
@@ -23,4 +23,68 @@ public class TreeNodeMCTS {
     this.wins = 0;
     this.nbVisits = 0;
   }
+
+  /**
+   * @return the number of wins that were observed
+   */
+  public int getNbWins() {
+    return this.wins;
+  }
+
+  /**
+   * @return the number of times this node was visited
+   */
+  public int getNbVisits() {
+    return this.nbVisits;
+  }
+
+  /**
+   * @return the list of children nodes for the current node
+   */
+  public List<TreeNodeMCTS> getChildrenNodes() {
+    return this.children;
+  }
+
+  /**
+   * @return the parent node of the current node
+   */
+  public TreeNodeMCTS getParentNode() {
+    return this.parent;
+  }
+
+  /**
+   * @return the GameState representing the current node
+   */
+  public GameState getGameState() {
+    return this.state;
+  }
+
+  /**
+   * Calculate UCT (Upper Confidence Bound for Trees)
+   *
+   * <p>UCT = w(i)/n(i) + c * sqrt(ln(t)/n(i))
+   *
+   * <p>w = number of wins after the i-th move (can be 0) n = number of simulations after the i-th
+   * move c = exploration parameter t = total number of simulations for the parent node
+   *
+   * @param node the child node
+   * @param exploration the exploration factor (c value in formula)
+   * @return The UCT value
+   */
+  private double uctValue(TreeNodeMCTS node, double exploration) {
+    if (node.nbVisits == 0) {
+      return Double.MAX_VALUE;
+    }
+    return (node.wins / (double) node.nbVisits)
+        + exploration * Math.sqrt(Math.log(this.nbVisits) / node.nbVisits);
+  }
+
+  /**
+   * Adds a child node to the current node (expand tree)
+   *
+   * @param child the child node wer want to add to the tree
+   */
+  public void addChildToTree(TreeNodeMCTS child) {
+    children.add(child);
+  }
 }
