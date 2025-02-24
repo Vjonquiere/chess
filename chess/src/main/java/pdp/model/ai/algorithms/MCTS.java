9bb18766ecfb9a5b2896224371a72b1794c3a09a
Jonathan Landry <jonathan.landry@etu.u-bordeaux.fr>
2025-02-24 15:49:45 +0100

    Adding simulate() method but needs refactor --> find a way to call game.PlayMove()

--- chess/src/main/java/pdp/model/ai/algorithms/MCTS.java
+++ chess/src/main/java/pdp/model/ai/algorithms/MCTS.java
@@ -1,12 +1,18 @@
 package pdp.model.ai.algorithms;
 
+import java.util.List;
+import java.util.Random;
 import pdp.model.Game;
+import pdp.model.GameState;
 import pdp.model.ai.AIMove;
 import pdp.model.ai.Solver;
+import pdp.model.board.Move;
+import pdp.model.piece.Color;
 
 public class MCTS implements SearchAlgorithm {
   Solver solver;
   private static final double EXPLORATION_FACTOR = Math.sqrt(2); // c value
+  private final Random random = new Random(); // Randomizer for the moves
 
   public MCTS(Solver solver) {
     this.solver = solver;
@@ -15,6 +21,28 @@ public class MCTS implements SearchAlgorithm {
   @Override
   public AIMove findBestMove(Game game, int depth, boolean player) {
     return null;
+  }
+
+  /**
+   * Assess the resulting position after a simulated sequence of moves
+   *
+   * @param state the state of the game
+   * @return the result of the simulation. -1 if black wins, 1 if white wins and 0 if draw
+   */
+  private int evaluateSimulation(GameState state) {
+    if (state.isGameOver()) {
+      if (state.getBoard().getBoardRep().isCheckMate(Color.WHITE)) {
+        // Black wins
+        return -1;
+      }
+      if (state.getBoard().getBoardRep().isCheckMate(Color.BLACK)) {
+        // White wins
+        return 1;
+      }
+      // Draw
+      return 0;
+    }
+    return 0;
   }
 
   /**
@@ -31,11 +59,50 @@ public class MCTS implements SearchAlgorithm {
   }
 
   // Expansion
-
-  // Simulation
+  // Generate child nodes until we reach a Terminal State
 
   /**
-   * Back propagates the obtained result during the algorithm
+   * Simulate a game randomly by playing moves randomly and assessing the sequence of played moves
+   *
+   * @param node the current node in the algorithm
+   * @return the evaluation of the simulated sequence of moves
+   */
+  private int simulate(TreeNodeMCTS node) {
+    GameState simulatedState = node.getGameState().getCopy();
+
+    while (!simulatedState.isGameOver()) {
+      List<Move> availableMoves =
+          simulatedState
+              .getBoard()
+              .getBoardRep()
+              .getAllAvailableMoves(simulatedState.isWhiteTurn());
+
+      if (availableMoves.isEmpty()) {
+        // No more legal moves so end
+        break;
+      }
+
+      Move randomMove = selectRandomMove(availableMoves);
+
+      // GAME.playMove()
+      simulatedState.getBoard().makeMove(randomMove);
+    }
+
+    return evaluateSimulation(simulatedState);
+  }
+
+  /**
+   * Selects a random move from the list of available moves
+   *
+   * @param moves the list of possible moves
+   * @return A randomly chosen move
+   */
+  private Move selectRandomMove(List<Move> moves) {
+    return moves.get(this.random.nextInt(moves.size()));
+  }
+
+  /**
+   * Back propagate the obtained result during the algorithm
    *
    * @param node the current tree node in the algorithm
    * @param result the obtained result after simulation
