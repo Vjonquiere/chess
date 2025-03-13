d50a5c7ddce2f61dd1415ed5ec22b3e2e3dec99b
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-03-13 14:31:04 +0100

    blitz in GUI

--- chess/src/main/java/pdp/view/GUI/controls/PlayerInfos.java
+++ chess/src/main/java/pdp/view/GUI/controls/PlayerInfos.java
@@ -1,5 +1,7 @@
 package pdp.view.GUI.controls;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -8,15 +10,25 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
+import pdp.model.Game;
+import pdp.utils.Timer;
 import pdp.view.GUIView;
 
 public class PlayerInfos extends HBox {
   Canvas currentPlayer;
+  Label timerLabel = new Label();
+  Timeline timeline;
 
   public PlayerInfos(String name, boolean isAI) {
     this.setAlignment(Pos.CENTER_LEFT);
     currentPlayer = new Canvas(20, 20);
-    this.getChildren().addAll(getPlayerIcon(isAI), new Label(name), currentPlayer);
+    Timer timer = Game.getInstance().getGameState().getMoveTimer();
+    if (timer != null) {
+      timerLabel.setText(timer.getTimeRemainingString());
+      updateTimer();
+    }
+    this.getChildren().addAll(getPlayerIcon(isAI), new Label(name), timerLabel, currentPlayer);
     this.setSpacing(10);
   }
 
@@ -31,12 +43,28 @@ public class PlayerInfos extends HBox {
     return imageView;
   }
 
+  public void updateTimer() {
+    timeline =
+        new Timeline(
+            new KeyFrame(
+                Duration.seconds(0.5),
+                event -> {
+                  Timer timer = Game.getInstance().getGameState().getMoveTimer();
+                  if (timer != null) {
+                    timerLabel.setText(timer.getTimeRemainingString());
+                  }
+                }));
+    timeline.setCycleCount(Timeline.INDEFINITE);
+  }
+
   public void setCurrentPlayer(boolean isCurrent) {
     GraphicsContext gc = currentPlayer.getGraphicsContext2D();
     gc.clearRect(0, 0, currentPlayer.getWidth(), currentPlayer.getHeight());
+    if (timeline != null) timeline.stop();
     if (isCurrent) {
       gc.setFill(Color.web(GUIView.theme.getAccent()));
       gc.fillOval(0, 0, 20, 20);
+      if (timeline != null) timeline.play();
     }
   }
 }
