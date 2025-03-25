70a955efce9951edae439124ca3af12c880eb7d9
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-03-25 21:32:06 +0100

    Adding tests for alpha-beta variants

--- chess/src/test/java/tests/AlphaBetaTest.java
+++ chess/src/test/java/tests/AlphaBetaTest.java
@@ -5,9 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import pdp.model.Game;
 import pdp.model.ai.AlgorithmType;
 import pdp.model.ai.HeuristicType;
@@ -41,8 +43,16 @@ public class AlphaBetaTest {
     game = Game.initialize(false, false, null, null, null, new HashMap<>());
   }
 
-  @Test
-  public void easyCase() {
+  static Stream<AlgorithmType> algorithmProvider() {
+    return Stream.of(
+        AlgorithmType.ALPHA_BETA, AlgorithmType.ALPHA_BETA_PARALLEL, AlgorithmType.ALPHA_BETA_ID);
+  }
+
+  @ParameterizedTest
+  @MethodSource("algorithmProvider")
+  public void easyCase(AlgorithmType algorithm) {
+    solver.setAlgorithm(algorithm);
+
     // Simulate Scholar's mate
     game.playMove(new Move(new Position(4, 1), new Position(4, 3)));
     game.playMove(new Move(new Position(4, 6), new Position(4, 4)));
@@ -50,14 +60,18 @@ public class AlphaBetaTest {
     game.playMove(new Move(new Position(1, 7), new Position(2, 5)));
     game.playMove(new Move(new Position(5, 0), new Position(2, 3)));
     game.playMove(new Move(new Position(6, 7), new Position(5, 5)));
+
     solver.playAiMove(game);
     assertTrue(game.getGameState().isGameOver());
   }
 
-  @Test
-  public void testTimerDefault() {
+  @ParameterizedTest
+  @MethodSource("algorithmProvider")
+  public void testTimerDefault(AlgorithmType algorithm) {
+    solver.setAlgorithm(algorithm);
     solver.setTime(5);
     solver.setDepth(5);
+
     long startTime = System.currentTimeMillis();
     solver.playAiMove(game);
     long endTime = System.currentTimeMillis();
@@ -69,8 +83,10 @@ public class AlphaBetaTest {
     assertTrue(remainingTime <= solver.getTime() * 1000);
   }
 
-  @Test
-  public void testTimer2s() {
+  @ParameterizedTest
+  @MethodSource("algorithmProvider")
+  public void testTimer2s(AlgorithmType algorithm) {
+    solver.setAlgorithm(algorithm);
     long timeLimit = 2;
     solver.setTime(timeLimit);
     solver.setDepth(5);
@@ -87,7 +103,8 @@ public class AlphaBetaTest {
     assertTrue(remainingTime <= timeLimit);
   }
   /*
-   @Test
+   @ParameterizedTest
+   @MethodSource("algorithmProvider")
    public void testTimerOverStartFunction() {
      long timeLimit = 1;
      solver.setDepth(20);
