6fe16e1caa8db91d0701b6e984f7304f663e8a88
Valentin Jonquiere <valentin.jonquiere@etu.u-bordeaux.fr>
2025-04-09 14:41:41 +0200

    bash update

--- chess/chess.sh
+++ chess/chess.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-JAR=chess-0.0.4.jar
+VERSION=$(grep -oPm1 "(?<=<version>)[^<]+" pom.xml)
 
-if [ ! -f "$JAR" ]; then
-    echo "$0: jar not found!"
-    exit 1
+JAR="./target/chess-${VERSION}.jar"
+
+if echo "$@" | grep -q -- "--clean"; then
+    echo "$0: Running mvn clean"
+    mvn clean
+    if [ $? -ne 0 ]; then
+        echo "$0: mvn clean failed!"
+        exit 1
+    fi
+    exit 0
 fi
 
-java -jar $JAR $@
+if [ ! -f "$JAR" ]; then
+    echo "$0: jar not found! Running mvn clean package..."
+    mvn clean package
+    if [ $? -ne 0 ]; then
+        echo "$0: mvn clean package failed!"
+        exit 1
+    fi
+fi
+
+java -jar $JAR "$@"
