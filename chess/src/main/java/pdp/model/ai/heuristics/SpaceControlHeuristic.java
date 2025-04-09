dfb21c4e37e0a08345cf2460a9604fd24f516be7
Iwen Jomaa <iwen.jomaa-gobbe@etu.u-bordeaux.fr>
2025-04-09 10:51:49 +0200

    Using correct weights

--- chess/src/main/java/pdp/model/ai/heuristics/SpaceControlHeuristic.java
+++ chess/src/main/java/pdp/model/ai/heuristics/SpaceControlHeuristic.java
@@ -1,5 +1,6 @@
 package pdp.model.ai.heuristics;
 
+import java.util.ArrayList;
 import java.util.List;
 import pdp.model.board.*;
 import pdp.utils.Position;
@@ -14,7 +15,7 @@ public class SpaceControlHeuristic implements Heuristic {
   private static final float SCORE_CAP = 100f;
 
   /** Maximum number of moves considered in the evaluation. */
-  private static final int MAX_MOVES_CONSIDERED = 50;
+  private static final int MAX_MOVES_CONSIDERED = 40;
 
   /** Bonus score for a move in the center of the board. */
   private static final float BONUS_MOVE_IN_CENTER = 3f;
@@ -25,8 +26,16 @@ public class SpaceControlHeuristic implements Heuristic {
   /** Bonus score for a move elsewhere on the board. */
   private static final float BONUS_MOVE_ELSEWHERE = 0.5f;
 
+  private static int MAX_MOVES_CENTER = 4;
+  private static int MAX_MOVES_FLANKS = 2 * 8;
+
   /** The multiplier used to keep the values under SCORE_CAP. */
-  private static final float MULTIPLIER = SCORE_CAP / (MAX_MOVES_CONSIDERED * BONUS_MOVE_ELSEWHERE);
+  private static final float MULTIPLIER =
+      SCORE_CAP
+          / ((MAX_MOVES_CENTER * BONUS_MOVE_IN_CENTER)
+              + (MAX_MOVES_FLANKS * BONUS_MOVE_ON_FLANKS)
+              + (MAX_MOVES_CONSIDERED
+                  - (MAX_MOVES_CENTER + MAX_MOVES_FLANKS) * BONUS_MOVE_ELSEWHERE));
 
   /** Minimum x-coordinate for the center region. */
   private static final int CENTER_X_MIN = 3;
@@ -41,10 +50,10 @@ public class SpaceControlHeuristic implements Heuristic {
   private static final int CENTER_Y_MAX = 4;
 
   /** Maximum x-coordinate for the left flank region. */
-  private static final int FLANK_LEFT_MAX_X = 1;
+  private static final int FLANK_LEFT_MAX_X = 0;
 
   /** Minimum x-coordinate for the right flank region. */
-  private static final int FLANK_RIGHT_MIN_X = 6;
+  private static final int FLANK_RIGHT_MIN_X = 7;
 
   /**
    * Gives a score based on how much control over the entire board the players have. Center is
@@ -59,7 +68,9 @@ public class SpaceControlHeuristic implements Heuristic {
     float score = 0;
     score += evaluateBoardControl(board, true) - evaluateBoardControl(board, false);
 
-    score = Math.min(score * MULTIPLIER, SCORE_CAP);
+    score = score * MULTIPLIER;
+
+    System.out.println("Score: " + score);
 
     return isWhite ? score : -score;
   }
@@ -72,35 +83,78 @@ public class SpaceControlHeuristic implements Heuristic {
    * @return a score based on board control
    */
   private float evaluateBoardControl(final BoardRepresentation board, final boolean isWhite) {
-    float score = 0;
+    int movesInCenter = 0;
+    int movesOnFlanks = 0;
+    int movesElsewhere = 0;
 
     if (board instanceof BitboardRepresentation bitboard) {
       Bitboard moves = bitboard.getColorAttackBitboard(isWhite);
       for (Integer moveSquare : moves.getSetBits()) {
         final Position posDest = BitboardUtils.squareToPosition(moveSquare);
         if (isInCenter(posDest)) {
-          score += BONUS_MOVE_IN_CENTER;
+          movesInCenter += 1;
         } else if (isInLeftFlank(posDest) || isInRightFlank(posDest)) {
-          score += BONUS_MOVE_ON_FLANKS;
+          movesOnFlanks += 1;
         } else {
-          score += BONUS_MOVE_ELSEWHERE;
+          movesElsewhere += 1;
         }
       }
-      return score;
+
+      return compileScores(movesInCenter, movesOnFlanks, movesElsewhere);
     }
 
     final List<Move> allPossibleMoves = board.getAllAvailableMoves(isWhite);
 
+    List<Position> toEval = new ArrayList<>();
     for (final Move move : allPossibleMoves) {
       final Position posDest = move.getDest();
+      if (toEval.contains(posDest)) {
+        continue;
+      }
+      toEval.add(posDest);
+    }
+
+    for (final Position posDest : toEval) {
       if (isInCenter(posDest)) {
-        score += BONUS_MOVE_IN_CENTER;
+        movesInCenter += 1;
       } else if (isInLeftFlank(posDest) || isInRightFlank(posDest)) {
-        score += BONUS_MOVE_ON_FLANKS;
+        movesOnFlanks += 1;
       } else {
-        score += BONUS_MOVE_ELSEWHERE;
+        movesElsewhere += 1;
       }
     }
+
+    return compileScores(movesInCenter, movesOnFlanks, movesElsewhere);
+  }
+
+  public float compileScores(int movesInCenter, int movesOnFlanks, int movesElsewhere) {
+    int movesConsidered = 0;
+    float score = 0;
+
+    if (movesConsidered + movesInCenter < MAX_MOVES_CONSIDERED) {
+      movesConsidered += movesInCenter;
+      score += movesInCenter * BONUS_MOVE_IN_CENTER;
+    } else {
+      score += (MAX_MOVES_CONSIDERED - movesConsidered) * BONUS_MOVE_IN_CENTER;
+      return score;
+    }
+
+    if (movesConsidered + movesOnFlanks < MAX_MOVES_CONSIDERED) {
+      movesConsidered += movesOnFlanks;
+      score += movesOnFlanks * BONUS_MOVE_ON_FLANKS;
+    } else {
+      score += (MAX_MOVES_CONSIDERED - movesConsidered) * BONUS_MOVE_ON_FLANKS;
+      return score;
+    }
+
+    if (movesConsidered + movesElsewhere < MAX_MOVES_CONSIDERED) {
+      movesConsidered += movesElsewhere;
+      score += movesElsewhere * BONUS_MOVE_ELSEWHERE;
+    } else {
+      score += (MAX_MOVES_CONSIDERED - movesConsidered) * BONUS_MOVE_ELSEWHERE;
+      return score;
+    }
+
     return score;
   }
 
@@ -112,7 +166,7 @@ public class SpaceControlHeuristic implements Heuristic {
         && pos.y() <= CENTER_Y_MAX;
   }
 
-  /** Checks if a position is in the left flank (files a-b). */
+  /** Checks if a position is in the left flank (files a-h). */
   private boolean isInLeftFlank(final Position pos) {
     return pos.x() <= FLANK_LEFT_MAX_X;
   }
